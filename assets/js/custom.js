e1da59801271f50e9c6e9a30c863e0e9ea31fd4f
blutooth <maximilian@live.co.uk>
2018-12-16 00:27:30 +0000

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,24 @@
+
+
+// Randomize profile order
+$( document ).ready(function() {
+	
+	(function($) {
+
+	$.fn.randomize = function(childElem) {
+	  return this.each(function() {
+		  var $this = $(this);
+		  var elems = $this.children(childElem);
+		  elems.sort(function() { return (Math.round(Math.random())-0.5); });  
+
+		  $this.remove(childElem);  
+
+		  for(var i=0; i < elems.length; i++)
+			$this.append(elems[i]);      
+
+	  });    
+	}
+	})(jQuery);
+
+	$("div.organizers").randomize("div.member");	
+});
